bbbc18d14d3aaf4ab485edfafd60df86b0f7fbd5
T.B.S <ewprjkt@proton.me>
2022-11-18 13:21:24 +0700

    update [skip ci]

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Project-Xtended/manifest.git -b xt -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Tiktodz/local_manifest --depth 1 -b xr .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/EternityOS-Plus-Tiramisu/manifest -b 13 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Tiktodz/local_manifest --depth 1 -b pe .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
-lunch xtended_X00TD-userdebug
+lunch aosp_X00TD-userdebug
 export TZ=Asia/Jakarta
-export SKIP_API_CHECKS=true
-export SELINUX_IGNORE_NEVERALLOWS=true
-make bacon -j8
+#export SKIP_API_CHECKS=true
+#export SELINUX_IGNORE_NEVERALLOWS=true
+mka bacon -j8
 # end
