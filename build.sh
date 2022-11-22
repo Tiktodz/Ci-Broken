9033822534fc5e422255c0e813e604365d4e51f8
T.B.S <ewprjkt@proton.me>
2022-11-22 21:25:00 +0700

    atest

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Project-Fluid/manifest.git -b fluid-12.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Tiktodz/local_manifest --depth 1 -b fluid .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 13.0 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Tiktodz/local_manifest --depth 1 -b crd .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
-lunch fluid_X00TD-userdebug
+lunch lineage_X00TD-userdebug
 export TZ=Asia/Jakarta
 #export SKIP_API_CHECKS=true
 #export SELINUX_IGNORE_NEVERALLOWS=true
